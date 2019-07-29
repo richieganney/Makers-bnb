f6568a34ac39b0c6d68292dfeb58a36c229f5210
Richie <richieganney@gmail.com>
2019-07-29 15:42:01 +0100

    fixing tests

--- spec/unit/spaces_spec.rb
+++ spec/unit/spaces_spec.rb
@@ -1,14 +1,15 @@
 require 'spaces'
 
 describe Spaces do
+  
   describe '.all' do
     it 'returns all spaces' do
-      DatabaseConnection.query("INSERT INTO spaces (title, owner) VALUES('Test Listing', 'test owner');")
+      DatabaseConnection.query("INSERT INTO spaces (title, owner) VALUES('Test Listing', 1);")
       spaces = Spaces.all
       expect(spaces.length).to eq 1
       expect(spaces.first).to be_a Spaces
       expect(spaces.first.title).to eq 'Test Listing'
-      expect(spaces.first.owner).to eq 'test owner'
+      expect(spaces.first.owner).to eq '1'
 
     end
   end
