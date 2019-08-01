b4cd72ddfe427548c1675b336b0e6735b40d124c
Richie <richieganney@gmail.com>
2019-08-01 15:44:17 +0100

    working on jquery

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
 
-}
+    $( function() {
+      $( "#available_from" ).datepicker({
+        dateFormat : 'mm-dd-yy',
+      });
+      $( "#available_to" ).datepicker({
+        dateFormat : 'mm-dd-yy',
+      });
+    });
+
+});
